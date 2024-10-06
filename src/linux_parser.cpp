1ae07532321e8213aab6626d49929450c1cfa0ef
ofgonen <ofgonen@gmail.com>
2024-10-06 18:39:20 +0300

    Process: Cpu Utilization

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -275,7 +275,7 @@ string LinuxParser::Uid(int pid) {
     }
   }
 
-  return string();
+  return value;
 }
 
 // DONE: Read and return the user associated with a process
@@ -313,11 +313,11 @@ long LinuxParser::UpTime(int pid) {
   // 1 (systemd) S 0 1 1 0 -1 4194560 9258 192992 109 2394 7233 486 576 209 20 0
   // 1 0 334 169865216 2813 18446744073709551615 1 1 0 0 0 0 671173123 4096 1260
   // 0 0 0 17 2 0 0 0 0 0 0 0 0 0 0 0 0 0
-  // 22nd value is the value we want!!
+  // 22nd value is the value we want which is the starttime of the process
+  // uptime = totaltime - starttime
 
   string line;
   string value;
-  string other;
 
   std::ifstream fileStream(kProcDirectory + std::to_string(pid) +
                            kStatFilename);
@@ -326,9 +326,7 @@ long LinuxParser::UpTime(int pid) {
 
     std::istringstream lineStream(line);
 
-    for (int i = 0; i < 21; i++) lineStream >> other;
-
-    lineStream >> value;
+    for (int i = 1; i <= 22; i++) lineStream >> value;
 
     long totalUp = UpTime();
     long seconds = std::stol(value) / sysconf(_SC_CLK_TCK);
@@ -339,4 +337,46 @@ long LinuxParser::UpTime(int pid) {
   return 0;
 }
 
-float LinuxParser::CpuUtilization(int pid) { return 0; }
+float LinuxParser::CpuUtilization(int pid) {
+  // /proc/pid/stat
+  // https://stackoverflow.com/questions/16726779/how-do-i-get-the-total-cpu-usage-of-an-application-from-proc-pid-stat/16736599#16736599
+  // #14 utime, #15 stime, #16 cutime, #17 cstime, #22 starttime
+
+  long utime = 0;      // #14
+  long stime = 0;      // #15
+  long startTime = 0;  // #22 starttime
+  long totalUptime = UpTime();
+  float usage = 0;
+
+  string line;
+  string value;
+
+  std::ifstream fileStream(kProcDirectory + std::to_string(pid) +
+                           kStatFilename);
+
+  if (fileStream.is_open()) {
+    std::getline(fileStream, line);
+    std::istringstream lineStream(line);
+
+    for (int i = 1; i <= 22; i++) {
+      lineStream >> value;
+      if (i == 14)
+        utime = std::stol(value);
+      else if (i == 15)
+        stime = std::stol(value);
+      else if (i == 22)
+        startTime = std::stol(value);
+    }
+
+    long totalTimeSpend = utime + stime;
+
+    long startTime_seconds = startTime / sysconf(_SC_CLK_TCK);
+    long totalTimeSpend_seconds = totalTimeSpend / sysconf(_SC_CLK_TCK);
+
+    long upTime = totalUptime - startTime_seconds;
+
+    if (upTime != 0) usage = totalTimeSpend_seconds / upTime;
+  }
+
+  return usage;
+}
